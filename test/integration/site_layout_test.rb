12c775696daa67730006a66146f95700428a5f3a
Bartosz Kita <bkita@afilias.info>
2016-12-06 15:36:53 +0000

    Add layout test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test 'home page layout' do
+    get root_path
+    assert_template 'static_pages/home'
+  end
+
+  test 'home page root link' do
+    get root_path
+    assert_select 'a[href=?]', root_path, count: 2
+  end
+
+  test 'home page help link' do
+    get root_path
+    assert_select 'a[href=?]', help_path
+  end
+
+  test 'home page contact link' do
+    get root_path
+    assert_select 'a[href=?]', contact_path
+  end
+
+  test 'home page about link' do
+    get root_path
+    assert_select 'a[href=?]', about_path
+  end
+end
