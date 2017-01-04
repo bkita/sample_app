637c579589d45e004794b887b3953f7bf07016f3
Bartosz Kita <bkita@afilias.info>
2017-01-04 11:18:44 +0000

    10.3.1 Users index - Finish

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:bartek)
+  end
+
   test 'home page layout' do
     get root_path
     assert_template 'static_pages/home'
     get contact_path
     assert_select 'title', full_title('Contact')
-
   end
 
   test 'home page root link' do
@@ -28,5 +31,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test 'home page about link' do
     get root_path
     assert_select 'a[href=?]', about_path
+  end
+
+  test 'links for logged user' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
   end
 end
