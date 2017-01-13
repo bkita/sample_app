d4e98cbf7909710267f542beaf302fdc1435a7aa
Bartosz Kita <bkita@afilias.info>
2017-01-13 16:27:39 +0000

    11.3.1 Generalizing the authenticated? method - Finished

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,11 @@
 class AccountActivationsController < ApplicationController
+
   def edit
+    user = User.find_by(email: params[:email])
+    if user && user.authenticated?(:activation, params[:id])
+      #Activate user
+    else
+      #Do something else
+    end
   end
 end
