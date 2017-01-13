30f0ca6233eed334ef405c9087b9982af4f5203a
Bartosz Kita <bkita@afilias.info>
2017-01-13 13:15:28 +0000

    11.2.4 Updating the Users create action - Finish

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -23,8 +23,8 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                          password: 'password',
                                          password_confirmation: 'password' } }
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.nil?
+    # follow_redirect!
+    # assert_template 'users/show'
+    # assert_not flash.nil?
   end
 end
