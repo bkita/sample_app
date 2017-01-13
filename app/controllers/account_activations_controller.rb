176ec48f17b5d8e109ff59d963c33987357da656
Bartosz Kita <bkita@afilias.info>
2017-01-13 17:18:20 +0000

    11.3.2 Activation edit action - Finish

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,9 +3,14 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && user.authenticated?(:activation, params[:id])
-      #Activate user
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in(user)
+      flash[:success] = 'Account activated!'
+      redirect_to user
     else
-      #Do something else
+      flash[:info] = 'Invalid token.'
+      redirect_to root_url
     end
   end
 end
