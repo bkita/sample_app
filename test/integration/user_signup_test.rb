0c9e8468dadd4297493f9dfbc8b361407167ee3c
Bartosz Kita <bartosz.kita@gmail.com>
2017-01-14 22:33:20 +0000

    Add user signup test with email and activation

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid user details' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -23,8 +27,24 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                          password: 'password',
                                          password_confirmation: 'password' } }
     end
-    # follow_redirect!
-    # assert_template 'users/show'
-    # assert_not flash.nil?
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path('invalid_token', email: user.email)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: 'invalid@email.com')
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
