fc6626413acb1e8cf1ca657206387844cf633416
Bartosz Kita <bkita@afilias.info>
2017-01-16 09:45:35 +0000

    Fix password_confirmation typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :passwrd_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def user_id
