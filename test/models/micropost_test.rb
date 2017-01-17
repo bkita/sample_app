e85612cd55060e23fd843337337e380b06190617
Bartosz Kita <bartosz.kita@gmail.com>
2017-01-17 00:02:27 +0000

    13.1.2 Micropost validations - Finish

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:bartek)
+    @micropost = Micropost.new(content: 'Lorem Ipsum',
+                               user_id: @user.id)
+  end
+
+  test 'micropost should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'content should be present' do
+    @micropost.content = ' '
+    assert_not @micropost.valid?
+  end
+
+  test 'content max length 140 chars' do
+    @micropost.content = 'a' * 141
+    assert_not @micropost.valid?
+  end
+end
