e0149e9331433a60de2677d0357eac2e104595dd
Bartosz Kita <bartosz.kita@gmail.com>
2017-01-20 22:29:46 +0000

    14.1.3 Relationship validations - Finish

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:bartek).id,
+                                     followed_id: users(:jo).id)
+  end
+
+
+
+  test 'follower id must be preset' do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test 'followed id must be preset' do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
